3985093a2da8b9a3fa4cc0e6d5485826455fdc7b
Carlos Dominguez Leyva <78928652+cedl3693@users.noreply.github.com>
2022-05-11 22:51:03 -0500

    1st commit lab sesion 5

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy en el elemento de navegacion principal
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+    };
+
+    // contraer  responsive navbar cuando el toggler es visible
+   const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
